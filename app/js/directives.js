eed2b1b41f48b26a1314d83254f0881d674a45dc
Richard Towers <richard_towers@Richard-Towers-Macbook.local>
2013-01-05 20:54:02 +0000

    Beginnings of simple jQuery UI drag and drop

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,6 +1,35 @@
 'use strict';
 
-/* Directives */
-
-
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', []).
+	directive('jquiDraggable', function () {
+		return {
+			link : function (scope, item, attrs) { 
+				item.draggable({
+					addClass: true,
+					revertDuration: 200,
+					revert: true
+				});
+			}
+		};
+	}).
+	directive('jquiDroppable', function () {
+		return {
+			link : function (scope, target, attrs) { 
+				target.droppable({
+					addClass: true,
+					over: function (event, ui) {
+						target.addClass('jqui-dnd-target-over');
+						ui.draggable.addClass('jqui-dnd-item-over');
+					},
+					out: function (event, ui) {
+						target.removeClass('jqui-dnd-target-over');
+						ui.draggable.removeClass('jqui-dnd-item-over');
+					},
+					drop: function () {
+						$('.jqui-dnd-target-over').removeClass('jqui-dnd-target-over');
+						$('.jqui-dnd-item-over').removeClass('jqui-dnd-item-over');
+					}
+				});
+			}
+		};
+	});
