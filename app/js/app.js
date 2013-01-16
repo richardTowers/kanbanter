8465df435af74740b0ea6832b46775663b9f0441
Richard Towers <richard_towers@Richard-Towers-Macbook.local>
2013-01-16 00:58:14 +0000

    Restructuring / Starting again from scratch.
    Drag and drop removed.
    Validation: hooray!
    Todo: TESTS.

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,18 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('kanbanter', ['kanbanter.filters', 'kanbanter.services', 'kanbanter.directives']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginController});
-    $routeProvider.when('/kanban', {templateUrl: 'partials/kanban.html', controller: KanbanController});
-    $routeProvider.otherwise({redirectTo: '/login'});
-  }]);
+angular.module('kanbanter', ['kanbanter.filters', 'kanbanter.services', 'kanbanter.directives'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginController});
+		$routeProvider.when('/', {templateUrl: 'partials/kanban.html', controller: KanbanController});
+		$routeProvider.otherwise({redirectTo: '/login'});
+	}])
+	.run(function ($rootScope, $location) {
+		// If there's already a user in localStorage, add them to the $rootScope.
+		if(window.localStorage) {
+			var user = JSON.parse(window.localStorage.getItem('user'));
+			if(user) {
+				$rootScope.user = user;
+			}
+		}
+	});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,147 +1,71 @@
 'use strict';
 
-// Some config, to be shared between the controllers:
-var assignee = {
-	unassigned : 3
-};
-var statuses = {
-	active: 1,
-	review: 7,
-	resolved: 3,
-	closed: 5
-};
 var redmineBaseUrl = 'http://redmine.richard-towers.com/';
 
-
 function LoginController($scope, $http, $rootScope, $location) {
-	function setData(apiCode, user) {
-		$rootScope.apiCode = apiCode;
+
+	var handleSuccessfulLogin = function handleSuccessfulLogin(user) {
+		user.apiCode = $scope.apiCode;
+		user.name = user.firstname + ' ' + user.lastname;
 		$rootScope.user = user;
-	}
-	if(window.localStorage) {
-		$scope.apiCode = window.localStorage.getItem('apiCode');
-		if($scope.apiCode != null) {
-			$http.get(redmineBaseUrl + 'users/current.json?key=' + $scope.apiCode).success(function (data) {
-				setData($scope.apiCode, data.user);
-				$location.path('/kanban');
-			});
+		if(window.localStorage) {
+			window.localStorage.setItem('user', JSON.stringify(user));
 		}
-	}
-	$scope.login = function() {
-		$http.get(redmineBaseUrl + 'users/current.json?key=' + $scope.apiCode).success(function (data) {
-			if(window.localStorage) {
-				window.localStorage.setItem('apiCode', $scope.apiCode);
-			}
-			setData($scope.apiCode, data.user);
-			$location.path('/kanban');
-		});
+		$location.path('/');
+	};
+
+	$scope.login = function login () {
+		// Make a get with the api code in the box to see if it's correct:
+		$http.get(redmineBaseUrl + 'users/current.json?key=' + $scope.apiCode)
+			.success(function (data) { handleSuccessfulLogin(data.user); })
+			.error(function () { alert('Oops! Something went wrong.'); });
+	};
+
+	// We might have to auto-login
+	if($rootScope.user) {
+		$location.path('/');
+		return;
 	}
 }
 LoginController.$inject = ['$scope', '$http', '$rootScope', '$location'];
 
-
 function KanbanController($scope, $http, $rootScope, $location) {
-	if($rootScope.apiCode == null) {
+
+	var handleLogout = function handleLogout () {
+		$rootScope.user = undefined;
+		if(window.localStorage) {
+			window.localStorage.removeItem('user');
+		}
 		$location.path('/login');
-		return;
-	}
-	var key = $rootScope.apiCode;
-	$http.get(redmineBaseUrl + 'users/current.json?key=' + key).success(function (data) {
-		$rootScope.apiCode = key;
-		$rootScope.user = data.user;
-	});
-	$scope.logout = function() {
-		localStorage.clear();
-		$location.path('/login');
-		return;
 	};
-	$scope.username = $rootScope.user.firstname + ' ' + $rootScope.user.lastname;
-	$http.get(redmineBaseUrl + 'issues.json?status_id=*&key='+key).success(function(data) {
-		var issues = data.issues;
-		// Backlog: Active and assigned to Dev Queue Unassigned:
-		$scope.backlog = _.filter(issues, function(x) {
-			return x.status.id === statuses.active && x.assigned_to.id === assignee.unassigned;
-		});
-		// Backlog: Active and not assigned to Dev Queue Unassigned:
-		$scope.inProgress = _.filter(issues, function(x) {
-			return x.status.id === statuses.active && x.assigned_to.id !== assignee.unassigned;
-		});
-		// Review: Status = Review and assigned to Dev Queue Unassigned:
-		$scope.review = _.filter(issues, function(x) {
-			return x.status.id === statuses.review && x.assigned_to.id === assignee.unassigned;
-		});
-		// ReviewDone: Status = Review and not assigned to Dev Queue Unassigned:
-		$scope.reviewDone = _.filter(issues, function(x) {
-			return x.status.id === statuses.review && x.assigned_to.id !== assignee.unassigned;
-		});
-		// InTesting: Status = Resolved
-		$scope.inTesting = _.filter(issues, function(x) {
-			return x.status.id === statuses.resolved;
-		});
-		// Complete: Status = Complete
-		$scope.complete = _.filter(issues, function(x) {
-			return x.status.id === statuses.closed;
-		});
-	}).error(function(data) {
-		console.log(data);
-		window.alert('Oh No! Something went wrong!');
-	});
-}
-KanbanController.$inject = ['$scope', '$http', '$rootScope', '$location'];
 
-function KanbanColumnController($scope, $http, $rootScope) {
-	$scope.handleDrop = function (ticketScope, endColumnScope) {
-		var userId = $rootScope.user.id;
-		var startColumnScope = ticketScope.$parent;
-		if(startColumnScope.title === endColumnScope.title) {
-			// Nothing to do:
-			return;
-		}
-
-		if(true){//window.confirm('This will update ticket #' + ticketScope.issue.id + '. Are you sure?')) {
-			var indexOfTicketToRemove = startColumnScope.issues.indexOf(ticketScope.issue);
-			startColumnScope.issues.splice(indexOfTicketToRemove, 1);
-			endColumnScope.issues.push(ticketScope.issue);
-			startColumnScope.$apply();
-			endColumnScope.$apply();
-
-			var assignedTo;
-			var statusId;
-			switch(endColumnScope.title) {
-				case 'Backlog':
-					assignedTo = assignee.unassigned;
-					statusId = statuses.active;
-					break;
-				case 'In Progress':
-					assignedTo = userId;
-					statusId = statuses.active;
-					break;
-				case 'Review':
-					assignedTo = assignee.unassigned;
-					statusId = statuses.review;
-					break;
-				case 'Review Done':
-					assignedTo = userId;
-					statusId = statuses.review;
-					break;
-				case 'In Testing':
-					assignedTo = userId;
-					statusId = statuses.resolved;
-					break;
-				case 'Complete':
-					assignedTo = null;
-					statusId = statuses.closed;
-					break;
-			}
-			var update = { issue : { status_id: statusId } };
-			if(assignedTo !== null) { update.issue.assigned_to_id = assignedTo; }
-
-			$http.put(redmineBaseUrl + 'issues/' + ticketScope.issue.id + '.json?key='+$rootScope.apiCode, update).
-				error(function (data) {
-					console.log(data);
-					window.alert('Oh No! Something went wrong!');
-				});
-		}
+	// Check that we have a valid user in context:
+	if(!$rootScope.user) {
+		handleLogout();
+		return;
 	}
+
+	// Try and use the user's apiCode to get the issues:
+	$http.get(redmineBaseUrl + 'issues.json?status_id=*&key=' + $rootScope.user.apiCode)
+		.success(function (data) { $scope.issues = data.issues; })
+		.error(function () { alert('Oh no.'); });
+
+	// Set up filters:
+	(function () {
+		var unassigned = 9;
+		var currentUser = $rootScope.user.id;
+		var active = 1;
+		var review = 7;
+		var resolved = 3;
+		var closed = 5;
+		$scope.backlog = function (ticket) { return ticket.assigned_to.id         === unassigned && ticket.status.id === active; }
+		$scope.inProgress = function (ticket) { return ticket.assigned_to.id      !== unassigned && ticket.status.id === active; }
+		$scope.developmentDone = function (ticket) { return ticket.assigned_to.id === unassigned && ticket.status.id === review; }
+		$scope.review = function (ticket) { return ticket.assigned_to.id          !== unassigned && ticket.status.id === review; }
+		$scope.reviewDone = function (ticket) { return ticket.assigned_to.id      === unassigned && ticket.status.id === resolved; }
+		$scope.inTesting = function (ticket) { return ticket.assigned_to.id       !== unassigned && ticket.status.id === resolved; }
+	})();
+
+	$scope.logout = handleLogout;
 }
-KanbanColumnController.$inject = ['$scope', '$http', '$rootScope'];
+LoginController.$inject = ['$scope', '$http', '$rootScope', '$location'];
