083d0caa564ab3f833011f43d2d22190a11717fd
Simon <s.shiharev@topface.com>
2014-06-16 19:29:59 +0400

    change rest method

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -16,7 +16,7 @@ function LoginController($scope, $http, $rootScope, $location) {
 
 	$scope.login = function login () {
 		// Make a get with the api code in the box to see if it's correct:
-		$http.get({method: 'GET', url: redmineBaseUrl + 'users/current.json?key=' + $scope.apiCode})
+		$http.get(redmineBaseUrl + 'users/current.json?key=' + $scope.apiCode)
 			.success(function (data) { handleSuccessfulLogin(data.user); })
 			.error(function () { alert('Oops! Something went wrong.'); });
 	};
