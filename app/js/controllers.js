67e1a3934cfa57136d3f1b4e7bd580b2b53ba8b0
Simon <s.shiharev@topface.com>
2014-07-08 17:02:06 +0400

    фиксы статусов

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -96,7 +96,7 @@ function KanbanController($scope, $http, $rootScope, $location) {
         //Неназначенные и низкоприоритетные
 		$scope.noDev = function (ticket) {
             return (!ticket.assigned_to || !ticket.assigned_to.id || (!ticket.priority || ticket.priority.id < 4
-                || ticket.priority.id == 21 || ticket.priority.id == 29))
+                || ticket.priority.id == 21 || ticket.priority.id == 22))
         }
 
         //Дизайн
@@ -112,7 +112,7 @@ function KanbanController($scope, $http, $rootScope, $location) {
                  && (ticket.tracker && ticket.tracker.id != designTracker)
                  && (ticket.priority && ticket.priority.id >= 4)
                  && (ticket.priority && ticket.priority.id != 21)
-                 && (ticket.priority && ticket.priority.id != 29)
+                 && (ticket.priority && ticket.priority.id != 22)
         }
 
         //Назначенные и в работе
